676b975e140e9e00b573f30d3eb8c6485116b3c5
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2015-11-10 18:19:53 -0600

    Initial commit

---
+++ ocr_db_alloc.cpp
@@ -0,0 +1,22 @@
+#include "ocr_db_alloc.hpp"
+
+#ifdef __APPLE__
+// For some reason they don't support the standard C++ thread_local,
+// but they *do* support the non-standard __thread extension for C.
+#define thread_local __thread
+#endif /* __APPLE__ */
+
+namespace Ocr {
+
+    static thread_local DatablockAllocator _localDbAllocator;
+
+    const DatablockAllocator &ocrAllocatorGet(void) {
+        return _localDbAllocator;
+    }
+
+    void ocrAllocatorSetDb(void *dbPtr, size_t dbSize, bool needsInit) {
+        new (&_localDbAllocator) DatablockAllocator(dbPtr, dbSize);
+        _localDbAllocator.init();
+    }
+
+}
